shuyangzhou/com-liferay-journal

commit cca23b47e74c6619ce11e72f7d309c21da3c18c6

LPS-36174 SF - avoid creating exceptions

journal-service/src/main/java/com/liferay/journal/service/permission/JournalFolderPermission.java
@@ -15,12 +15,13 @@
 package com.liferay.journal.service.permission;
 
 import com.liferay.exportimport.kernel.staging.permission.StagingPermissionUtil;
-import com.liferay.journal.exception.NoSuchFolderException;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.model.JournalFolderConstants;
 import com.liferay.journal.service.JournalFolderLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -67,9 +68,8 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, JournalFolder folder,
-			String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, JournalFolder folder,
+		String actionId) {
 
 		String portletId = PortletProviderUtil.getPortletId(
 			JournalArticle.class.getName(), PortletProvider.Action.EDIT);
@@ -90,25 +90,29 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 		if (actionId.equals(ActionKeys.VIEW) &&
 			PropsValues.PERMISSIONS_VIEW_DYNAMIC_INHERITANCE) {
 
-			try {
-				long folderId = folder.getFolderId();
+			long folderId = folder.getFolderId();
 
-				while (folderId !=
-							JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			while (folderId !=
+						JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
 
-					folder = _journalFolderLocalService.getFolder(folderId);
+				folder = _journalFolderLocalService.fetchFolder(folderId);
 
+				if (folder != null) {
 					if (!_hasPermission(permissionChecker, folder, actionId)) {
 						return false;
 					}
+				}
+				else {
+					if (!folder.isInTrash()) {
+						_log.error(
+							"Unable to obtain JournalFolder with folderId" +
+								folderId);
 
-					folderId = folder.getParentFolderId();
+						return false;
+					}
 				}
-			}
-			catch (NoSuchFolderException nsfe) {
-				if (!folder.isInTrash()) {
-					throw nsfe;
-				}
+
+				folderId = folder.getParentFolderId();
 			}
 
 			return JournalPermission.contains(
@@ -119,17 +123,22 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, long groupId, long folderId,
-			String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, long groupId, long folderId,
+		String actionId) {
 
 		if (folderId == JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
 			return JournalPermission.contains(
 				permissionChecker, groupId, actionId);
 		}
 		else {
-			JournalFolder folder = _journalFolderLocalService.getJournalFolder(
+			JournalFolder folder = _journalFolderLocalService.fetchFolder(
 				folderId);
+
+			if (folder == null) {
+				_log.error("Unable to obtain Folder with folderId" + folderId);
+
+				return false;
+			}
 
 			return contains(permissionChecker, folder, actionId);
 		}
@@ -167,6 +176,9 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 
 		return false;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalFolderPermission.class);
 
 	private static JournalFolderLocalService _journalFolderLocalService;
 

journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -299,7 +299,8 @@ public class JournalContentDisplayContext {
 		catch (PortalException pe) {
 			_log.error(
 				"Unable to obtain ddm template for article " +
-					articleDisplay.getId());
+					articleDisplay.getId(),
+				pe);
 		}
 
 		return _ddmTemplate;
@@ -355,7 +356,8 @@ public class JournalContentDisplayContext {
 		catch (PortalException pe) {
 			_log.error(
 				"Unable to obtain ddm temmplate for article " +
-					article.getId());
+					article.getId(),
+				pe);
 		}
 
 		return _ddmTemplates;
@@ -798,16 +800,9 @@ public class JournalContentDisplayContext {
 		ThemeDisplay themeDisplay = (ThemeDisplay)_portletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		try {
-			_showEditArticleIcon = JournalArticlePermission.contains(
-				themeDisplay.getPermissionChecker(), latestArticle.getGroupId(),
-				latestArticle.getArticleId(), ActionKeys.UPDATE);
-		}
-		catch (PortalException pe) {
-			_log.error(
-				"Unable to check permissions for article " +
-					latestArticle.getId());
-		}
+		_showEditArticleIcon = JournalArticlePermission.contains(
+			themeDisplay.getPermissionChecker(), latestArticle.getGroupId(),
+			latestArticle.getArticleId(), ActionKeys.UPDATE);
 
 		return _showEditArticleIcon;
 	}
@@ -839,7 +834,8 @@ public class JournalContentDisplayContext {
 		catch (PortalException pe) {
 			_log.error(
 				"Unable to check permission on ddm template " +
-					ddmTemplate.getTemplateId());
+					ddmTemplate.getTemplateId(),
+				pe);
 		}
 
 		return _showEditTemplateIcon;

journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_0/UpgradeImageTypeContent.java
@@ -22,6 +22,8 @@ import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Image;
 import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
@@ -170,6 +172,10 @@ public class UpgradeImageTypeContent extends UpgradeProcess {
 				groupId, folderId, fileName);
 		}
 		catch (PortalException pe) {
+			_log.error(
+				"Unable to obtain fileEntry with groupId " + groupId +
+					", folderId " + folderId + " and fileName " + fileName,
+				pe);
 		}
 
 		return fileEntry;
@@ -256,6 +262,9 @@ public class UpgradeImageTypeContent extends UpgradeProcess {
 			}
 		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeImageTypeContent.class);
 
 	private final ImageLocalService _imageLocalService;
 

journal-service/src/main/java/com/liferay/journal/model/impl/JournalArticleImpl.java
@@ -211,33 +211,55 @@ public class JournalArticleImpl extends JournalArticleBaseImpl {
 	public String getContentByLocale(String languageId) {
 		Map<String, String> tokens = new HashMap<>();
 
-		try {
-			DDMStructure ddmStructure = getDDMStructure();
+		DDMStructure ddmStructure = getDDMStructure();
 
+		if (ddmStructure != null) {
 			tokens.put(
 				"ddm_structure_id",
 				String.valueOf(ddmStructure.getStructureId()));
-		}
-		catch (PortalException pe) {
 		}
 
 		return getContentByLocale(getDocument(), languageId, tokens);
 	}
 
 	@Override
-	public DDMStructure getDDMStructure() throws PortalException {
-		return DDMStructureLocalServiceUtil.fetchStructure(
-			PortalUtil.getSiteGroupId(getGroupId()),
-			ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class),
-			getDDMStructureKey(), true);
+	public DDMStructure getDDMStructure() {
+		DDMStructure ddmStructure = null;
+
+		try {
+			ddmStructure = DDMStructureLocalServiceUtil.fetchStructure(
+				PortalUtil.getSiteGroupId(getGroupId()),
+				ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class),
+				getDDMStructureKey(), true);
+		}
+		catch (PortalException pe) {
+			_log.error(
+				"Unable to find DDM Structure with DDMStructureKey " +
+					getDDMStructureKey(),
+				pe);
+		}
+
+		return ddmStructure;
 	}
 
 	@Override
-	public DDMTemplate getDDMTemplate() throws PortalException {
-		return DDMTemplateLocalServiceUtil.fetchTemplate(
-			PortalUtil.getSiteGroupId(getGroupId()),
-			ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class),
-			getDDMStructureKey(), true);
+	public DDMTemplate getDDMTemplate() {
+		DDMTemplate ddmTemplate = null;
+
+		try {
+			ddmTemplate = DDMTemplateLocalServiceUtil.fetchTemplate(
+				PortalUtil.getSiteGroupId(getGroupId()),
+				ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class),
+				getDDMStructureKey(), true);
+		}
+		catch (PortalException pe) {
+			_log.error(
+				"Unable to find DDM Template for DDM Structure with " +
+					"DDMStructureKey " + getDDMStructureKey(),
+				pe);
+		}
+
+		return ddmTemplate;
 	}
 
 	@JSON

journal-service/src/main/java/com/liferay/journal/service/permission/JournalArticlePermission.java
@@ -16,13 +16,15 @@ package com.liferay.journal.service.permission;
 
 import com.liferay.exportimport.kernel.staging.permission.StagingPermissionUtil;
 import com.liferay.journal.configuration.JournalServiceConfiguration;
-import com.liferay.journal.exception.NoSuchFolderException;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.model.JournalFolderConstants;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.service.JournalFolderLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
@@ -111,9 +113,8 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, JournalArticle article,
-			String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, JournalArticle article,
+		String actionId) {
 
 		String portletId = PortletProviderUtil.getPortletId(
 			JournalArticle.class.getName(), PortletProvider.Action.EDIT);
@@ -145,10 +146,22 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 			}
 		}
 
-		JournalServiceConfiguration journalServiceConfiguration =
-			_configurationProvider.getCompanyConfiguration(
-				JournalServiceConfiguration.class,
-				permissionChecker.getCompanyId());
+		JournalServiceConfiguration journalServiceConfiguration = null;
+
+		try {
+			journalServiceConfiguration =
+				_configurationProvider.getCompanyConfiguration(
+					JournalServiceConfiguration.class,
+					permissionChecker.getCompanyId());
+		}
+		catch (ConfigurationException ce) {
+			_log.error(
+				"Unable to obtain Company Configuration for Journal Service " +
+					"and Company " + permissionChecker.getCompanyId(),
+				ce);
+
+			return false;
+		}
 
 		if (actionId.equals(ActionKeys.VIEW) &&
 			!journalServiceConfiguration.articleViewPermissionsCheckEnabled()) {
@@ -169,10 +182,10 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 				}
 			}
 			else {
-				try {
-					JournalFolder folder = _journalFolderLocalService.getFolder(
-						folderId);
+				JournalFolder folder = _journalFolderLocalService.fetchFolder(
+					folderId);
 
+				if (folder != null) {
 					if (!JournalFolderPermission.contains(
 							permissionChecker, folder, ActionKeys.ACCESS) &&
 						!JournalFolderPermission.contains(
@@ -181,9 +194,13 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 						return false;
 					}
 				}
-				catch (NoSuchFolderException nsfe) {
+				else {
 					if (!article.isInTrash()) {
-						throw nsfe;
+						_log.error(
+							"Unable to obtain JournalFolder with folderId" +
+								folderId);
+
+						return false;
 					}
 				}
 			}
@@ -202,48 +219,74 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, long classPK, String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, long classPK, String actionId) {
 
 		JournalArticle article = _journalArticleLocalService.fetchLatestArticle(
 			classPK);
 
 		if (article == null) {
-			article = _journalArticleLocalService.getArticle(classPK);
+			article = _journalArticleLocalService.fetchArticle(classPK);
+
+			if (article == null) {
+				_log.error("Unable to find JournalArticle with id" + classPK);
+
+				return false;
+			}
 		}
 
 		return contains(permissionChecker, article, actionId);
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, long groupId, String articleId,
-			double version, String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, long groupId, String articleId,
+		double version, String actionId) {
 
-		JournalArticle article = _journalArticleLocalService.getArticle(
+		JournalArticle article = _journalArticleLocalService.fetchArticle(
 			groupId, articleId, version);
 
+		if (article == null) {
+			_log.error(
+				"Unable to find JournalArticle with groupId " + groupId +
+					", articleId " + articleId + " and version " + version);
+
+			return false;
+		}
+
 		return contains(permissionChecker, article, actionId);
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, long groupId, String articleId,
-			int status, String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, long groupId, String articleId,
+		int status, String actionId) {
 
-		JournalArticle article = _journalArticleLocalService.getLatestArticle(
+		JournalArticle article = _journalArticleLocalService.fetchLatestArticle(
 			groupId, articleId, status);
 
+		if (article == null) {
+			_log.error(
+				"Unable to find JournalArticle with groupId " + groupId +
+					", articleId " + articleId + " and status " + status);
+
+			return false;
+		}
+
 		return contains(permissionChecker, article, actionId);
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, long groupId, String articleId,
-			String actionId)
-		throws PortalException {
+		PermissionChecker permissionChecker, long groupId, String articleId,
+		String actionId) {
 
-		JournalArticle article = _journalArticleLocalService.getArticle(
+		JournalArticle article = _journalArticleLocalService.fetchArticle(
 			groupId, articleId);
+
+		if (article == null) {
+			_log.error(
+				"Unable to find JournalArticle with groupId " + groupId +
+					"and articleId " + articleId);
+
+			return false;
+		}
 
 		return contains(permissionChecker, article, actionId);
 	}
@@ -277,6 +320,9 @@ public class JournalArticlePermission implements BaseModelPermissionChecker {
 
 		_journalFolderLocalService = journalFolderLocalService;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalArticlePermission.class);
 
 	private static ConfigurationProvider _configurationProvider;
 	private static JournalArticleLocalService _journalArticleLocalService;

journal-service/src/main/java/com/liferay/journal/verify/JournalServiceVerifyProcess.java
@@ -333,17 +333,25 @@ public class JournalServiceVerifyProcess extends VerifyLayout {
 		long folderId = GetterUtil.getLong(pathArray[3]);
 		String title = HttpUtil.decodeURL(HtmlUtil.escape(pathArray[4]));
 
-		try {
-			FileEntry fileEntry = _dlAppLocalService.getFileEntry(
-				groupId, folderId, title);
+		FileEntry fileEntry = null;
 
-			Node node = dynamicContentElement.node(0);
+		try {
+			fileEntry = _dlAppLocalService.getFileEntry(
+				groupId, folderId, title);
+		}
+		catch (PortalException pe) {
+			_log.error(
+				"Unable to obtain fileEntry with groupId " + groupId +
+					", folderId " + folderId + " and title " + title,
+				pe);
+
+			return;
+		}
+
+		Node node = dynamicContentElement.node(0);
 
 			node.setText(
 				context + path + StringPool.SLASH + fileEntry.getUuid());
-		}
-		catch (PortalException pe) {
-		}
 	}
 
 	protected void updateDynamicElements(JournalArticle article)

commit cad3ea236ab2644c4deb7b70eb31cd0e3e9c9bd6

LPS-36174 Fix regression caused by cca23b47e74c6619ce11e72f7d309c21da3c18c6

journal-service/src/main/java/com/liferay/journal/service/permission/JournalFolderPermission.java
@@ -95,6 +95,8 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 			while (folderId !=
 						JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
 
+				JournalFolder parentFolder = folder;
+
 				folder = _journalFolderLocalService.fetchFolder(folderId);
 
 				if (folder != null) {
@@ -103,7 +105,12 @@ public class JournalFolderPermission implements BaseModelPermissionChecker {
 					}
 				}
 				else {
-					if (!folder.isInTrash()) {
+					if (parentFolder.isInTrash()) {
+						folder = parentFolder;
+
+						break;
+					}
+					else {
 						_log.error(
 							"Unable to obtain JournalFolder with folderId" +
 								folderId);