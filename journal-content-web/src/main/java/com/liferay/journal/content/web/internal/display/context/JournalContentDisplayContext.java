5ed6986d14085fb5dfeb06a30e8267b8543a0ce9
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-27 09:15:19 -0700

    LPS-71519 Avoid unnecessary refetch

--- journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
+++ journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -667,8 +667,7 @@ public class JournalContentDisplayContext {
 					WebKeys.THEME_DISPLAY);
 
 			_hasViewPermission = JournalArticlePermission.contains(
-				themeDisplay.getPermissionChecker(), article.getGroupId(),
-				article.getArticleId(), ActionKeys.VIEW);
+				themeDisplay.getPermissionChecker(), article, ActionKeys.VIEW);
 		}
 
 		return _hasViewPermission;
@@ -837,8 +836,8 @@ public class JournalContentDisplayContext {
 			WebKeys.THEME_DISPLAY);
 
 		_showEditArticleIcon = JournalArticlePermission.contains(
-			themeDisplay.getPermissionChecker(), latestArticle.getGroupId(),
-			latestArticle.getArticleId(), ActionKeys.UPDATE);
+			themeDisplay.getPermissionChecker(), latestArticle,
+			ActionKeys.UPDATE);
 
 		return _showEditArticleIcon;
 	}
