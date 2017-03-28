shuyangzhou/com-liferay-journal

commit 3c49b4020c9c025f816171ede3591368b55a251e

LPS-69740 Adds new method to get transformers by className

journal-api/src/main/java/com/liferay/journal/util/JournalTransformerListenerRegistry.java
@@ -23,6 +23,8 @@ import java.util.List;
  */
 public interface JournalTransformerListenerRegistry {
 
+	public TransformerListener getTransformerListener(String className);
+
 	public List<TransformerListener> getTransformerListeners();
 
 }

journal-service/src/main/java/com/liferay/journal/transformer/DefaultJournalTransformerListenerRegistryImpl.java
@@ -36,6 +36,11 @@ public class DefaultJournalTransformerListenerRegistryImpl
 	implements JournalTransformerListenerRegistry {
 
 	@Override
+	public TransformerListener getTransformerListener(String className) {
+		return _transformerListeners.get(className);
+	}
+
+	@Override
 	public List<TransformerListener> getTransformerListeners() {
 		return new ArrayList(_transformerListeners.values());
 	}

journal-service/src/main/java/com/liferay/journal/transformer/JournalTransformerListenerRegistryUtil.java
@@ -27,6 +27,14 @@ import org.osgi.util.tracker.ServiceTracker;
  */
 public class JournalTransformerListenerRegistryUtil {
 
+	public static TransformerListener getTransformerListener(String className) {
+		JournalTransformerListenerRegistry journalTransformerListenerRegistry =
+			_serviceTracker.getService();
+
+		return journalTransformerListenerRegistry.getTransformerListener(
+			className);
+	}
+
 	public static List<TransformerListener> getTransformerListeners() {
 		JournalTransformerListenerRegistry journalTransformerListenerRegistry =
 			_serviceTracker.getService();

commit 85671d337ed1e5404d8bd56203bda9ab46d07e62

LPS-69740 This should be a ProviderType with minor verioning bumping, caused by 3c49b4020c9c025f816171ede3591368b55a251e

journal-api/src/main/java/com/liferay/journal/util/JournalTransformerListenerRegistry.java
@@ -14,6 +14,8 @@
 
 package com.liferay.journal.util;
 
+import aQute.bnd.annotation.ProviderType;
+
 import com.liferay.portal.kernel.templateparser.TransformerListener;
 
 import java.util.List;
@@ -21,6 +23,7 @@ import java.util.List;
 /**
  * @author Pavel Savinov
  */
+@ProviderType
 public interface JournalTransformerListenerRegistry {
 
 	public TransformerListener getTransformerListener(String className);

journal-api/src/main/resources/com/liferay/journal/util/packageinfo
@@ -1,1 +1,1 @@
-version 1.1.0
+version 1.2.0